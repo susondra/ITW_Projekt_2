7e9394eeab62cbd3dcb34b87c1be379a8401c315
Ondra <o.sustr@post.cz>
2024-04-28 13:35:03 +0200

    hotovo

--- js/nav_menu_rozbaleni.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('#toggle_menu').click(function () {
-        $('#nav_menu').toggleClass('open'); // Přepínání třídy "open"
-    });
-});
--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -4,12 +4,13 @@ $(document).ready(function () {
             event.preventDefault();
             var offset = ($(window).width() < 1024) ? 78 : 0; // Určení, zda má být použit offset
             const hash = this.hash;
+            const speed = 800;
             TargetPosition = $(hash).offset().top - offset;
             $('html, body').animate(
                 {
                     scrollTop: TargetPosition
                 },
-                800, // rychlost animace
+                speed, // rychlost animace
                 function () {
                     window.location.hash = hash - offset;
                 }
